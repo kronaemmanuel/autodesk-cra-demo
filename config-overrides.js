229b4a151d51654fed370a9d2b7fda993589f218
Krona Emmanuel <kronaemmanuel@gmail.com>
2023-08-02 01:19:18 +0500

    Add babel-plugin-proposal-private-property-in-object

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,13 @@
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
+  const babelLoader = config.module.rules
+    .find(rule => 'oneOf' in rule)
+    .oneOf.find(
+      rule => rule.loader && rule.loader.includes('babel-loader'),
+    );
+
+  if (babelLoader && babelLoader.options) {
+    babelLoader.options.plugins.push(['@babel/plugin-proposal-private-property-in-object', { "loose": true }]);
+  }
+
   return config;
 };
